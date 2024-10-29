a38ca0a752bcad5919bcd89c0c1afee34c5a17eb
sahuPiyuS <piyushsahu.20009@gmail.com>
2024-10-29 05:52:37 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as never version
+
+//alert(3+3); // we are using nodejs, not browser
+
+console.log(3+
+    3) // code  readability should be high
+
+    console.log("piyush")
+
+
+    let name = "piyush"
+    let age = 18
+    let isloggedin = false
+
+
+
+  // number => 2 to power 53
+    // bigint 
+    // string => ""
+    // booleam => true?false
+    // null => standalone value
+    // undefined => value is not defined yet
+    // symbol => unique
+
+
+    // object
+
+    console.log(typeof age)
